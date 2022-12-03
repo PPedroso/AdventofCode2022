f2424ce58c46099e111237cd9cdd9a099100aec5
PPedroso <pedroso_pedro@hotmail.com>
2022-12-03 19:40:39 +0000

    Add project files.

---
+++ AdventofCode2022/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Console.WriteLine($"Day one - {DayOne()}");
+Console.ReadLine();
+
+
+int DayOne()
+{
+    var input = File.ReadAllText("../../../inputs/dayone.txt");
+
+    var elves = input.Split(Environment.NewLine);
+
+    var max = 0;
+    var total = 0;
+
+    foreach (var elf in elves)
+    {
+        if (String.IsNullOrEmpty(elf))
+        {
+            if (total > max)
+                max = total;
+            total = 0;
+        }
+
+        else
+            total += Int32.Parse(elf);
+    }
+
+    return max;
+}
+
