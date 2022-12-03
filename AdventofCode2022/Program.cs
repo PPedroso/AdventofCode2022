80129bc3a82c2c0836023041549a660f4df4a88b
PPedroso <pedroso_pedro@hotmail.com>
2022-12-03 20:06:46 +0000

    Solve day 1 puzzles

--- AdventofCode2022/Program.cs
+++ AdventofCode2022/Program.cs
@@ -1,21 +1,23 @@
 ﻿
 
-Console.WriteLine($"Day one - {DayOne()}");
+using System.Collections.Immutable;
+
+Console.WriteLine($"Day one - {DayOneB()}");
 Console.ReadLine();
 
 
-int DayOne()
+int DayOneA()
 {
-    var input = File.ReadAllText("../../../inputs/dayone.txt");
+    var input = File.ReadAllText("../../../inputs/dayone_example.txt");
 
     var elves = input.Split(Environment.NewLine);
 
     var max = 0;
     var total = 0;
 
-    foreach (var elf in elves)
+    foreach (var calories in elves)
     {
-        if (String.IsNullOrEmpty(elf))
+        if (String.IsNullOrEmpty(calories))
         {
             if (total > max)
                 max = total;
@@ -23,9 +25,41 @@ int DayOne()
         }
 
         else
-            total += Int32.Parse(elf);
+            total += Int32.Parse(calories);
     }
 
     return max;
+}
+
+int DayOneB()
+{
+    var input = File.ReadAllText("../../../inputs/dayone.txt");
+    var elves = input.Split(Environment.NewLine);
+
+    int[] maxCalories = new int[3] { 0, 0, 0 };
+    var total = 0;
+
+    foreach (var calories in elves)
+    {
+        if (String.IsNullOrEmpty(calories))
+        {
+            if (total > maxCalories[0])
+            {
+                maxCalories[0] = total;
+                Array.Sort(maxCalories);
+            }
+
+
+            total = 0;
+        }
+        else
+            total += Int32.Parse(calories);
+    }
+
+    if (total > maxCalories[0])
+        maxCalories[0] = total;
+
+    return maxCalories.Sum();
+
 }
 
