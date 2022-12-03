7ef82cb726b7a657c8f37c42f9bc2b2f47216322
PPedroso <pedroso_pedro@hotmail.com>
2022-12-03 21:56:03 +0000

    Solve day 2

--- AdventofCode2022/Program.cs
+++ AdventofCode2022/Program.cs
@@ -1,65 +1,164 @@
-﻿
+﻿using System.Data;
+using static AdventofCode2022.Utils;
 
-using System.Collections.Immutable;
-
-Console.WriteLine($"Day one - {DayOneB()}");
+Console.WriteLine($"Day two - {Main.DayTwoB()}");
 Console.ReadLine();
 
-
-int DayOneA()
+public static class Main
 {
-    var input = File.ReadAllText("../../../inputs/dayone_example.txt");
+    #region DayOne
 
-    var elves = input.Split(Environment.NewLine);
-
-    var max = 0;
-    var total = 0;
-
-    foreach (var calories in elves)
+    internal static int DayOneA()
     {
-        if (String.IsNullOrEmpty(calories))
+        var input = GetFileInput("dayone.txt");
+
+        var elves = input.Split(Environment.NewLine);
+
+        var max = 0;
+        var total = 0;
+
+        foreach (var calories in elves)
         {
-            if (total > max)
-                max = total;
-            total = 0;
-        }
-
-        else
-            total += Int32.Parse(calories);
-    }
-
-    return max;
-}
-
-int DayOneB()
-{
-    var input = File.ReadAllText("../../../inputs/dayone.txt");
-    var elves = input.Split(Environment.NewLine);
-
-    int[] maxCalories = new int[3] { 0, 0, 0 };
-    var total = 0;
-
-    foreach (var calories in elves)
-    {
-        if (String.IsNullOrEmpty(calories))
-        {
-            if (total > maxCalories[0])
+            if (String.IsNullOrEmpty(calories))
             {
-                maxCalories[0] = total;
-                Array.Sort(maxCalories);
+                if (total > max)
+                    max = total;
+                total = 0;
             }
 
-
-            total = 0;
+            else
+                total += Int32.Parse(calories);
         }
-        else
-            total += Int32.Parse(calories);
+
+        return max;
     }
 
-    if (total > maxCalories[0])
-        maxCalories[0] = total;
+    internal static int DayOneB()
+    {
+        var input = GetFileInput("dayone.txt");
+        var elves = input.Split(Environment.NewLine);
 
-    return maxCalories.Sum();
+        int[] maxCalories = new int[3] { 0, 0, 0 };
+        var total = 0;
+
+        foreach (var calories in elves)
+        {
+            if (String.IsNullOrEmpty(calories))
+            {
+                if (total > maxCalories[0])
+                {
+                    maxCalories[0] = total;
+                    Array.Sort(maxCalories);
+                }
+
+
+                total = 0;
+            }
+            else
+                total += Int32.Parse(calories);
+        }
+
+        if (total > maxCalories[0])
+            maxCalories[0] = total;
+
+        return maxCalories.Sum();
+
+    }
+
+    #endregion DayOne
+
+    #region DayTwo
+
+    internal static int DayTwoA()
+    {
+        var inputs = GetFileInput("daytwo.txt").Split(Environment.NewLine);
+
+        int total = 0;
+
+        foreach (var play in inputs)
+        {
+            var choices = play
+                            .Replace("X", "A")
+                            .Replace("Y", "B")
+                            .Replace("Z", "C")
+                            .Split(" ")
+                            .Select(x => Char.Parse(x))
+                            .ToArray();
+
+            int result = DoesFirstPlayerWin(choices[1], choices[0]);
+            if (result > 0) total += choices[1].ToScore() + 6;
+            if (result == 0) total += choices[1].ToScore() + 3;
+            if (result < 0) total += choices[1].ToScore();
+        }
+
+        return total;
+    }
+
+    internal static int DayTwoB()
+    {
+
+        var inputs = GetFileInput("daytwo.txt").Split(Environment.NewLine);
+
+        int total = 0;
+
+        foreach (var play in inputs)
+        {
+            var choices = play
+                            .Split(" ")
+                            .Select(x => Char.Parse(x))
+                            .ToArray();
+
+            var enemyChoice = choices[0];
+            var desiredOutcome = choices[1];
+
+            char complementaryCharacter = GetComplementaryCharacter(enemyChoice, desiredOutcome);
+            int result = DoesFirstPlayerWin(complementaryCharacter, enemyChoice);
+            if (result > 0) total += complementaryCharacter.ToScore() + 6;
+            if (result == 0) total += complementaryCharacter.ToScore() + 3;
+            if (result < 0) total += complementaryCharacter.ToScore();
+        }
+
+        return total;
+    }
+
+    static int ToScore(this Char c) => ((int)c) - 64;
+
+    /// <summary>
+    /// A - Rock, B - Paper, C - Scisors
+    /// 1 for win, 0 for draw, -1 for loss
+    /// </summary>
+    static int DoesFirstPlayerWin(char mine, char his)
+    {
+        if (mine == his) return 0;
+        if (mine == 'A' && his == 'C') return 1; //Rock(A) beats Scisors(C)) 
+        if (mine == 'B' && his == 'A') return 1; //Paper(B) beats Rock(A) 
+        if (mine == 'C' && his == 'B') return 1; //Scisors(C) beats Paper(B
+        return -1;
+    }
+
+    static Char Beats(this Char c)
+    {
+        if (c == 'A') return 'C';
+        if (c == 'B') return 'A';
+        return 'B';
+    }
+
+    static Char IsBeatenBy(this Char c)
+    {
+        if (c == 'C') return 'A';
+        if (c == 'A') return 'B';
+        return 'C';
+    }
+
+    static Char GetComplementaryCharacter(Char choice, Char outcome)
+    {
+        if (outcome == 'X') return choice.Beats();
+        if (outcome == 'Y') return choice;
+        return choice.IsBeatenBy();
+    }
+
+    #endregion DayTwo
 
 }
+
 
---
+++ AdventofCode2022/Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventofCode2022
+{
+    internal static class Utils
+    {
+        internal static string GetFileInput(string filePath)
+        {
+            return File.ReadAllText($"../../../inputs/{filePath}");
+        }
+    }
+}
