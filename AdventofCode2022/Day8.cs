70f739b3d608c04c60cfd72a1bd61686207f419d
PPedroso <pedroso_pedro@hotmail.com>
2022-12-23 06:58:11 +0000

    Finishes day8 a

---
+++ AdventofCode2022/Day8.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Day8
+{
+    public static string GetColumn(char[][] matrix, int column)
+    {
+        var chars = new List<char>();
+
+        foreach (var row in matrix)
+        {
+            chars.Add(row[column]);
+        }
+
+        return new string(chars.ToArray());
+    }
+
+
+    public static int DayEightA(string filePath)
+    {
+        var matrix = File.ReadAllLines($"../../../inputs/{filePath}").Select(x => x.ToArray()).ToArray();
+
+        var totalTrees = 0;
+        var j = 0;
+        var i = 0;
+
+        for (; i < matrix.Length; ++i)
+        {
+            var row = new string(matrix[i]);
+
+            for (; j < matrix.Length; ++j)
+            {
+                var column = GetColumn(matrix, j);
+                if (column.Substring(0, i).CanBeSeen(matrix[i][j]) ||
+                    column.Substring(i+1).CanBeSeen(matrix[i][j]) ||
+                    row.Substring(0, j).CanBeSeen(matrix[i][j]) ||
+                    row.Substring(j+1).CanBeSeen(matrix[i][j])) {
+                    totalTrees++;
+                }
+                
+
+            }
+            j = 0;
+        }
+        return totalTrees;
+    }
+
+    static bool CanBeSeen(this string str, char tree)
+    {
+        if (string.IsNullOrWhiteSpace(str)) return true;
+        return str.All(x => int.Parse(x.ToString()) < int.Parse(tree.ToString()));
+    }
+
+    public static int DayEightB(string filePath)
+    {
+        var matrix = File.ReadAllLines($"../../../inputs/{filePath}").Select(x => x.ToArray()).ToArray();
+
+        var totalTrees = 0;
+        var j = 0;
+        var i = 0;
+
+        for (; i < matrix.Length; ++i)
+        {
+            var row = new string(matrix[i]);
+
+            for (; j < matrix.Length; ++j)
+            {
+                var column = GetColumn(matrix, j);
+                if (column.Substring(0, i).CanBeSeen(matrix[i][j]) ||
+                    column.Substring(i + 1).CanBeSeen(matrix[i][j]) ||
+                    row.Substring(0, j).CanBeSeen(matrix[i][j]) ||
+                    row.Substring(j + 1).CanBeSeen(matrix[i][j]))
+                {
+                    totalTrees++;
+                }
+
+
+            }
+            j = 0;
+        }
+        return totalTrees;
+    }
+
+    private int GetScenicScore() {
+        return 0;
+    }
+}
--- AdventofCode2022/Program.cs
+++ AdventofCode2022/Program.cs
@@ -1,25 +1,30 @@
 ﻿using static AdventofCode2022.Utils;
 
-string input = GetFileInput("day1.txt");
-Console.WriteLine($"Day one - (A) {Day1.DayOneA(input)} - (B) {Day1.DayOneB(input)}");
+string input = string.Empty;
 
-input = GetFileInput("day2.txt");
-Console.WriteLine($"Day two - (A) {Day2.DayTwoA(input)} - (B) {Day2.DayTwoB(input)}");
+//input = GetFileInput("day1.txt");
+//Console.WriteLine($"Day one - (A) {Day1.DayOneA(input)} - (B) {Day1.DayOneB(input)}");
 
-input = GetFileInput("day3.txt");
-Console.WriteLine($"Day three - (A) {Day3.DayThreeA(input)} - (B) {Day3.DayThreeB(input)}");
+//input = GetFileInput("day2.txt");
+//Console.WriteLine($"Day two - (A) {Day2.DayTwoA(input)} - (B) {Day2.DayTwoB(input)}");
 
-input = GetFileInput("day4.txt");
-Console.WriteLine($"Day four - (A) {Day4.DayFourA(input)} - (B) {Day4.DayFourB(input)}");
+//input = GetFileInput("day3.txt");
+//Console.WriteLine($"Day three - (A) {Day3.DayThreeA(input)} - (B) {Day3.DayThreeB(input)}");
 
-input = GetFileInput("day5.txt");
-Console.WriteLine($"Day five - (A) {Day5.DayFiveA(input)} - (B) {Day5.DayFiveB(input)}");
+//input = GetFileInput("day4.txt");
+//Console.WriteLine($"Day four - (A) {Day4.DayFourA(input)} - (B) {Day4.DayFourB(input)}");
 
-input = GetFileInput("day6.txt");
-Console.WriteLine($"Day six - (A) {Day6.DaySixA(input)} - (B) {Day6.DaySixB(input)}");
+//input = GetFileInput("day5.txt");
+//Console.WriteLine($"Day five - (A) {Day5.DayFiveA(input)} - (B) {Day5.DayFiveB(input)}");
 
-input = GetFileInput("day7.txt");
-Console.WriteLine($"Day seven - (A) {Day7.DaySevenA(input)} - (B) {Day7.DaySevenB(input)}");
+//input = GetFileInput("day6.txt");
+//Console.WriteLine($"Day six - (A) {Day6.DaySixA(input)} - (B) {Day6.DaySixB(input)}");
+
+//input = GetFileInput("day7.txt");
+//Console.WriteLine($"Day seven - (A) {Day7.DaySevenA(input)} - (B) {Day7.DaySevenB(input)}");
+
+input = GetFileInput("day8_example.txt");
+Console.WriteLine($"Day eight - (A) {Day8.DayEightA("day8.txt")} - (B) {Day8.DayEightB("day8.txt")}");
 
 
 Console.ReadLine();
